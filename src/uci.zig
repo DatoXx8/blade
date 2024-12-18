98bec0e0b11073d72694256b43e3601ec72b0647
DatoXx8 <datoxx8@gmail.com>
2024-12-18 17:43:35 +0100

    Factor out encoding step for testing purposes

--- src/uci.zig
+++ src/uci.zig
@@ -13,49 +13,53 @@ const stdin = std.io.getStdIn().reader();
 const stdout = std.io.getStdOut().writer();
 
 pub const Uci = struct {
+    fn encode(from: u8, to: u8) [4]u8 {
+        return [4]u8{
+            switch (File.of(from)) {
+                .fa => 'a',
+                .fb => 'b',
+                .fc => 'c',
+                .fd => 'd',
+                .fe => 'e',
+                .ff => 'f',
+                .fg => 'g',
+                .fh => 'h',
+            },
+            switch (Rank.of(from)) {
+                .r1 => '1',
+                .r2 => '2',
+                .r3 => '3',
+                .r4 => '4',
+                .r5 => '5',
+                .r6 => '6',
+                .r7 => '7',
+                .r8 => '8',
+            },
+            switch (File.of(to)) {
+                .fa => 'a',
+                .fb => 'b',
+                .fc => 'c',
+                .fd => 'd',
+                .fe => 'e',
+                .ff => 'f',
+                .fg => 'g',
+                .fh => 'h',
+            },
+            switch (Rank.of(to)) {
+                .r1 => '1',
+                .r2 => '2',
+                .r3 => '3',
+                .r4 => '4',
+                .r5 => '5',
+                .r6 => '6',
+                .r7 => '7',
+                .r8 => '8',
+            },
+        };
+    }
     pub fn write(move: Move) !void {
-        try stdout.print("{s}{s}{s}{s}\n", .{
-            switch (File.of(move.from)) {
-                .fa => "a",
-                .fb => "b",
-                .fc => "c",
-                .fd => "d",
-                .fe => "e",
-                .ff => "f",
-                .fg => "g",
-                .fh => "h",
-            },
-            switch (Rank.of(move.from)) {
-                .r1 => "1",
-                .r2 => "2",
-                .r3 => "3",
-                .r4 => "4",
-                .r5 => "5",
-                .r6 => "6",
-                .r7 => "7",
-                .r8 => "8",
-            },
-            switch (File.of(move.to)) {
-                .fa => "a",
-                .fb => "b",
-                .fc => "c",
-                .fd => "d",
-                .fe => "e",
-                .ff => "f",
-                .fg => "g",
-                .fh => "h",
-            },
-            switch (Rank.of(move.to)) {
-                .r1 => "1",
-                .r2 => "2",
-                .r3 => "3",
-                .r4 => "4",
-                .r5 => "5",
-                .r6 => "6",
-                .r7 => "7",
-                .r8 => "8",
-            },
-        });
+        // TODO: Maybe do `catch unreachable` here??
+        try stdout.print("{s}\n", .{encode(move.from, move.to)});
     }
     /// Split because this part is tested sepperately
     fn parse(board: *const Board, buffer: [5]u8) Move {
