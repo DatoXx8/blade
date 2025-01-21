8c0de955fe1a4cc7df5c2aa3dc2952b899ff4967
DatoXx8 <datoxx8@gmail.com>
2025-01-21 20:12:05 +0100

    Main file for testing

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,33 @@
 const std = @import("std");
 
+const Board = @import("./board.zig").Board;
+const Piece = @import("./board.zig").Piece;
+const Movelist = @import("./move.zig").Movelist;
+const move_count_max = @import("./move.zig").move_count_max;
+const Move = @import("./move.zig").Move;
+
+const Uci = @import("./uci.zig").Uci;
+
+// TODO: Follow Zig style guide https://ziglang.org/documentation/master/#Style-Guide (NEEDED!!!)
+// TODO: Follow the encoding approach from that Data Oriented Design Talk (NEEDED!!!)
+//
+// TODO: Figure out how to make the directory structure for tests nicer
+//  Seems to only be possible via simulation_test_uci.root_module.addImport(name: []const u8, module: *Module)
+//
+// TODO: Figure out if it makes sense to do all the logic for castling perms and the like in the moves / movegen or just have
+//  moves be a from and to square and then do the logic in the makeMove function
+//
+//  IDEAS: Have a mixture of experts type things for different size nets decided by a very small net (nnue net run on the cpu?)
+
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var board: Board = Board.alloc("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    var movelist: Movelist = Movelist.init();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    movelist.generate(&board);
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    board.print();
 
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    try Uci.write(movelist.move[3]);
+    const move: Move = Uci.read(&board);
+    move.print();
 }
